fd584234add8644adadaea0f23117d1315ded0a5
Darlan Fernandes <fernandes.darlan@gmail.com>
2025-11-17 23:41:52 -0300

    update project

--- database/database.py
+++ database/database.py
@@ -1,5 +1,7 @@
+import sqlite3
+
 class PontoColeta:
-    def __init__(self, id=None, nome=None, endereco=None, cidade=None, estado=None, 
+    def __init__(self, id=None, nome=None, endereco=None, cidade=None, estado=None,
                  telefone=None, horario_funcionamento=None, tipos_materiais=None):
         self.id = id
         self.nome = nome
@@ -9,3 +11,27 @@ class PontoColeta:
         self.telefone = telefone
         self.horario_funcionamento = horario_funcionamento
         self.tipos_materiais = tipos_materiais
+
+def criar_tabela():
+    """
+    Cria a tabela de pontos de coleta se não existir
+    """
+    conn = sqlite3.connect('pontos_coleta.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS pontos_coleta (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            endereco TEXT NOT NULL,
+            cidade TEXT NOT NULL,
+            estado TEXT NOT NULL,
+            telefone TEXT,
+            horario_funcionamento TEXT NOT NULL,
+            tipos_materiais TEXT NOT NULL
+        )
+    ''')
+    
+    conn.commit()
+    conn.close()
+    print("Tabela 'pontos_coleta' criada/verificada com sucesso!")
